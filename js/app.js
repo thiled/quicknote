57c39d6a42d591586fece7028dbc5144df9e5944
Linfeng Gao <bluekylin@gmail.com>
2019-11-15 02:46:02 +0800

    还原菜单模块分离

--- js/app.js
+++ js/app.js
@@ -1,9 +1,47 @@
-import {
-  contextMenu,
-  projectListMenu,
-  themesMenu,
-  newProjectMenu
-} from './context-menu.js';
+// menu definition
+let themesMenu = {
+  name: '颜色主题',
+  menu: [
+    {
+      name: 'Breeze',
+      bgColor: '#232627',
+      fontColor: '#fcfcfc'
+    },
+    {
+      name: 'Monokai',
+      bgColor: '#272822',
+      fontColor: '#a6e22e'
+    },
+    {
+      name: 'Light',
+      bgColor: '#fff',
+      fontColor: '#000'
+    },
+    {
+      name: 'Terminal',
+      bgColor: '#000',
+      fontColor: '#18f018'
+    },
+    {
+      name: 'VSCode',
+      bgColor: '#1e1e1e',
+      fontColor: '#9cdcfe'
+    }
+  ],
+  focused: ''
+};
+let projectListMenu = {
+  name: '项目列表',
+  menu: [],
+  focused: ''
+};
+let newProjectMenu = {
+  name: '新建项目'
+};
+
+// 上下文菜单，支持多级
+let contextMenu = [newProjectMenu, projectListMenu, themesMenu];
+
 const ThemeKey = 'quickNote_theme';
 const storeTheme = window.localStorage.getItem(ThemeKey);
 
